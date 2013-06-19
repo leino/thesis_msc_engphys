8442ffa05bfa67be354f7674cd0d31044c9840a3
Anders Leino <anders.leino@gmail.com>
2013-06-19 21:17:06 +0300

    added more plays

--- experiments/runstrangeclass.sh
+++ experiments/runstrangeclass.sh
@@ -6,10 +6,11 @@ time runhaskell addhypergraphs.hs strangeclass.db 3 2 -d1:2 -D2:2
 for NUM_ITERATIONS in {10..300..10}
 do
     echo adding experiments for UCT$NUM_ITERATIONS...
-    time runhaskell addexperiments.hs strangeclass.db UCT$NUM_ITERATIONS Perfect 10
+    time runhaskell addexperiments.hs strangeclass.db UCT$NUM_ITERATIONS Perfect 100
 done
 
 echo planning...
 time runhaskell plan.hs strangeclass.db
+
 echo playing...
 time runhaskell -i../poga play.hs strangeclass.db
