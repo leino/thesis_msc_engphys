8758efb081c2d3e4c70f47ce34a613da368ed810
Anders Leino <anders.leino@gmail.com>
2013-07-23 21:26:15 +0300

    some cosmetic changes

--- experiments/run.py
+++ experiments/run.py
@@ -7,16 +7,19 @@ min_edge_size, max_edge_size = 2, 3
 tournament_size = 100
 
 iteration_stepsize = 1
-min_iterations, max_iterations = 2, 20
+min_iterations, max_iterations = 2, 30
 
 if len(sys.argv) >= 2:
     database_filename = sys.argv[1]
 
-    print('adding hypergraphs into {0}...'.format(database_filename))
+    print('adding some hypergraphs into {0}...'.format(database_filename))
 
     # sketch out the hypergraph class
-    
-    subprocess.check_call(["time", "runhaskell", "addhypergraphs.hs", database_filename, str(num_vertices), str(num_edges), '-d1:%(min_edge_size)d' % {'min_edge_size': min_edge_size}, '-D%(num_edges)d:%(max_edge_size)d' % {'num_edges': num_edges, 'max_edge_size': max_edge_size}])
+
+    args_hypergraphs = ["time", "runhaskell", "addhypergraphs.hs", database_filename, str(num_vertices), str(num_edges), '-d1:%(min_edge_size)d' % {'min_edge_size': min_edge_size}, '-D%(num_edges)d:%(max_edge_size)d' % {'num_edges': num_edges, 'max_edge_size': max_edge_size}]
+    subprocess.check_call(args_hypergraphs)
+
+    print("adding some experiments")
 
     # add experiments for perfect play
     subprocess.check_call(["time", "runhaskell", "addexperiments.hs", database_filename, "Perfect", "Perfect"])
@@ -25,6 +28,8 @@ if len(sys.argv) >= 2:
     for i in range(min_iterations, max_iterations, iteration_stepsize):
         first_player_desc = 'UCT%(num_iterations)d' % {'num_iterations': i}
         subprocess.check_call(["time", "runhaskell", "addexperiments.hs", database_filename, first_player_desc, "Perfect", str(tournament_size)])
+
+    print("plan and play")
 
     # plan and play (takes a long time)
     subprocess.check_call(["time", "runhaskell", "plan.hs", database_filename])
