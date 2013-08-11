54c92e0352eef650b5b4fa462cdc8a17262e108c
Anders Leino <anders.leino@gmail.com>
2013-08-11 21:55:13 +0300

    removed the annoying time output from all process invocations

--- experiments/run.py
+++ experiments/run.py
@@ -28,7 +28,7 @@ if len(sys.argv) >= 2:
         print('adding some hypergraphs into {0}...'.format(database_filename))
 
         # sketch out the hypergraph class
-        args_hypergraphs = ["time", "runhaskell", "addhypergraphs.hs", \
+        args_hypergraphs = ["runhaskell", "addhypergraphs.hs", \
                                 database_filename, str(num_vertices), str(num_edges), \
                                 '-d1:%(min_edge_size)d' % {'min_edge_size': min_edge_size}, \
                                 '-D%(num_edges)d:%(max_edge_size)d' % {'num_edges': num_edges, 'max_edge_size': max_edge_size}]
@@ -37,18 +37,18 @@ if len(sys.argv) >= 2:
     print("adding some experiments")
         
     # add experiments for perfect play
-    subprocess.check_call(["time", "runhaskell", "addexperiments.hs", database_filename, "Perfect", "Perfect"])
+    subprocess.check_call(["runhaskell", "addexperiments.hs", database_filename, "Perfect", "Perfect"])
 
     # add experiments for mcts vs perfect, for various numbers of iterations
     for i in range(min_iterations, max_iterations, iteration_stepsize):
         first_player_desc = 'UCT%(num_iterations)d' % {'num_iterations': i}
-        subprocess.check_call(["time", "runhaskell", "addexperiments.hs", database_filename, first_player_desc, "Perfect", str(tournament_size)])
+        subprocess.check_call(["runhaskell", "addexperiments.hs", database_filename, first_player_desc, "Perfect", str(tournament_size)])
 
     print("plan and play")
 
     # plan and play (takes a long time)
-    subprocess.check_call(["time", "runhaskell", "plan.hs", database_filename])
-    subprocess.check_call(["time", "runhaskell", "-i../poga", "play.hs", database_filename])
+    subprocess.check_call(["runhaskell", "plan.hs", database_filename])
+    subprocess.check_call(["runhaskell", "-i../poga", "play.hs", database_filename])
 
 else:
     print("you need to specify a database filename")
