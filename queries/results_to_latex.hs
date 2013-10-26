da89afb9cf0e5f02251e4aa895b0d5c4a566de48
Anders Leino <anders.leino@gmail.com>
2013-10-26 19:38:16 +0300

    now parsing actual data from rows

--- queries/results_to_latex.hs
+++ queries/results_to_latex.hs
@@ -6,15 +6,19 @@ import System.Environment (getArgs)
 import Text.Trifecta
 import Text.Trifecta.Parser
 import Text.Trifecta.Delta (Delta (..))
-import Control.Applicative ((<$>))
+import Control.Applicative
 
 data Winner = First | Neither deriving (Show, Read)
 
+newtype Column = Column (Maybe (String, String, String)) deriving Show
+
+newtype Row = Row [Column] deriving Show
+
 -- holds a single table in the results file
-data Table = Table { winner :: Winner, rows :: [String] } deriving Show
+data Table = Table { winner :: Winner, rows :: [Row] } deriving Show
 
 -- holds all of the tables in the results file
-data ResultsFile = ResultsFile [Table]
+newtype ResultsFile = ResultsFile [Table]
 
 parseWinner :: Parser Winner
 parseWinner = read <$> ( choice $ map (string . show) [First, Neither] )
@@ -25,12 +29,33 @@ resultsTable = do
   string "winner: "
   w <- parseWinner
   newline
-  let nonemptyLine = do
-        s <- some $ noneOf ['\n']
-        newline
-        return s
-  (some $ nonemptyLine) >>= return . Table w
-
+  -- skip the "header column" containing column numbers  
+  nonemptyLine
+  -- the rows containing data
+  (some $ row) >>= return . Table w
+  where
+    nonemptyLine = (some $ noneOf ['\n']) >> newline
+    column = do
+      choice [
+        try $ (\[a,b,c] -> Column $ Just (a,b,c)) <$> (count 3 entry), -- try to parse data column
+        (optional $ many $ char ' ') >> (return $ Column Nothing) -- try to parse empty column
+        ]
+      where
+        entry = do
+          optional space
+          s <- some $ digit <|> char '.'
+          optional space
+          optional $ char ','
+          optional space
+          return $ s
+    row = do
+      -- skip first column (contains only row number)
+      manyTill (noneOf ['\n']) (char '|')
+      -- read in actual data columns
+      cs <- column `sepEndBy1` (char '|')
+      newline
+      return $ Row cs
+  
 resultsFileParser :: Parser ResultsFile
 resultsFileParser = ResultsFile <$> resultsTable `sepEndBy1` newline
 
@@ -43,6 +68,8 @@ parse p s = parseString p (Columns 0 0) s
 main = do
   [resultsFile] <- getArgs
   contents <- readFile resultsFile
-  let Success (ResultsFile tables) = parse resultsFileParser contents
-  print $ head tables
+  case parse resultsFileParser contents of
+    Success (ResultsFile tables) -> do
+      print $ head tables
+    Failure err -> print err
   
