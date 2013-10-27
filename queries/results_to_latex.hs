9951b28ff7e3f87da678bcc14a23541ca9906d35
Anders Leino <anders.leino@gmail.com>
2013-10-27 21:40:22 +0200

    made rough outline of what needs to be done to convert into latex

--- queries/results_to_latex.hs
+++ queries/results_to_latex.hs
@@ -7,12 +7,14 @@ import Text.Trifecta
 import Text.Trifecta.Parser
 import Text.Trifecta.Delta (Delta (..))
 import Control.Applicative
+import Data.List (transpose, intersperse)
+import Control.Monad (sequence)
 
-data Winner = First | Neither deriving (Show, Read)
+data Winner = First | Neither deriving (Show, Read, Eq)
 
-newtype Column = Column (Maybe (String, String, String)) deriving Show
+newtype Entry = Entry { entry :: Maybe (String, String, String) } deriving Show
 
-newtype Row = Row [Column] deriving Show
+newtype Row = Row {entries :: [Entry]} deriving Show
 
 -- holds a single table in the results file
 data Table = Table { winner :: Winner, rows :: [Row] } deriving Show
@@ -37,8 +39,8 @@ resultsTable = do
     nonemptyLine = (some $ noneOf ['\n']) >> newline
     column = do
       choice [
-        try $ (\[a,b,c] -> Column $ Just (a,b,c)) <$> (count 3 entry), -- try to parse data column
-        (optional $ many $ char ' ') >> (return $ Column Nothing) -- try to parse empty column
+        try $ (\[a,b,c] -> Entry $ Just (a,b,c)) <$> (count 3 entry), -- try to parse data column
+        (optional $ many $ char ' ') >> (return $ Entry Nothing) -- try to parse empty column
         ]
       where
         entry = do
@@ -65,11 +67,53 @@ tableToLatex tbl = ""
 parse :: Parser a -> String -> Result a
 parse p s = parseString p (Columns 0 0) s
 
+-- interleave rows from the tables
+interleaveRows :: [Table] -> [[Row]]
+interleaveRows = transpose . map rows
+
+-- combine a list of rows to a single row of lists of entries
+combineRows :: [Row] -> [ Maybe [(String, String, String)] ]
+combineRows = map (sequence . map entry) . (transpose . map entries)
+
+-- make a big result table from a list of tables
+processTables :: [Table] -> [[Maybe [(String, String, String)]]]
+processTables = map combineRows . interleaveRows
+
+-- convert a single column of a result table row into latex
+columnLatex :: Maybe [(String, String, String)] -> String
+columnLatex Nothing = ""
+columnLatex (Just triples) =
+  concat . intersperse "\n" $
+  [ header, 
+    concat . intersperse "\\\n" . map tripleLatex $ triples,
+    footer ]
+  where
+    header = undefined -- TODO: should be some kind of table
+    footer = undefined
+    tripleLatex (a,b,c) = concat $ intersperse ", " $ [a, b, c]
+
+-- convert row of result table into latex
+rowLatex :: [Maybe [(String, String, String)]] -> String
+rowLatex = concat . intersperse "&&\n" . map columnLatex
+
+-- conver the whole result table to latex
+tableLatex :: [[Maybe [(String, String, String)]]] -> String
+tableLatex rs =
+  concat . intersperse "\n" $
+  [ header,
+    concat . intersperse "\\\n" . map rowLatex $ rs,
+    footer ]
+  where
+    header = undefined -- TODO: should be some kind of table
+    footer = undefined
+
 main = do
   [resultsFile] <- getArgs
   contents <- readFile resultsFile
   case parse resultsFileParser contents of
     Success (ResultsFile tables) -> do
-      print $ head tables
+      let firstWinnerTables = filter ((==) First . winner) tables
+          neitherWinnerTables = filter ((==) Neither . winner) tables
+      print $ head $ processTables neitherWinnerTables
     Failure err -> print err
   
